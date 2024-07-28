79a161000c4207392c95707b1b34a59fe4800946
TEMHITHORPHE <tehmhithorphe@gmail.com>
2024-07-28 18:08:59 +0100

    minor UI fix

--- src/components/TransactionHistory/HistoryButton.jsx
+++ src/components/TransactionHistory/HistoryButton.jsx
@@ -2,12 +2,14 @@
 
 
 import "./historybutton.css";
+import HistoryIcon from "../../img/history-2.svg";
+
 
 function HistoryButton() {
   return (
     <div id="history-floating-button">
         <div id="content">
-            History
+            <img src={HistoryIcon} alt="" width={60} height={60} />
         </div>
     </div>
   )
